69330a9570a78903973bb6a713d63a6752ec9189
Sneppy <andrea.mecchia@mail.polimi.it>
2018-05-19 13:22:21 +0200

    Major changes

--- src/SnooPHP/Instagram/Api.php
+++ src/SnooPHP/Instagram/Api.php
@@ -31,6 +31,11 @@ class Api
 	protected $token;
 
 	/**
+	 * @var string $lastResult last request result (raw)
+	 */
+	protected $lastResult;
+
+	/**
 	 * @var string $version api version (default: v1)
 	 */
 	protected $version = "v1";
@@ -41,9 +46,23 @@ class Api
 	protected $cacheClass = "SnooPHP\Instagram\NullCache";
 
 	/**
+	 * @var string $defaultCacheClass
+	 */
+	protected static $defaultCacheClass = "SnooPHP\Facebook\NullCache";
+
+	/**
 	 * @const ENDPOINT instagram api endpoint
 	 */
 	const ENDPOINT = "https://api.instagram.com";
+
+	/**
+	 * Create a new instance
+	 */
+	public function __construct()
+	{
+		// Set cache class
+		$this->cacheClass = static::$defaultCacheClass;
+	}
 
 	/**
 	 * Perform a generic query
@@ -55,21 +74,47 @@ class Api
 	public function query($query)
 	{
 		// If no access token, abort
-		if (empty($this->token)) return false;
+		if (empty($this->token))
+			return false;
+		else
+			$token = $this->token;
+
+		// Build uri
+		$uri	= preg_match("/^https?:\/\//", $query) ? $query : static::ENDPOINT."/{$this->version}/{$query}";
+		$uri	.= (strpos($query, '?') !== false ? '&' : '?')."access_token=$token";
 
 		// Check if cached result exists
-		if ($record = $this->cacheClass::fetch($query, $this->token)) return $record;
+		if ($record = $this->cacheClass::fetch("$uri|$token")) return $record;
 
 		// Make api request
 		$curl = new Get(static::ENDPOINT."/{$this->version}/{$query}".(strpos($query, '?') !== false ? '&' : '?')."access_token={$this->token}");
 		if ($curl && $curl->success())
 		{
 			// Save record in cache and return it
-			$raw = $curl->content();
-			return $this->cacheClass::store($query, $this->token, $raw);
+			$this->lastResult = $curl->content();
+			return $this->cacheClass::store("$uri|$token", $this->lastResult);
 		}
 		else
+		{
+			$this->lastResult = false;
 			return false;
+		}
+	}
+
+	/**
+	 * Create a new instance from client id and secret
+	 * 
+	 * @param string	$clientId		client id
+	 * @param string	$clientSecret	client secret
+	 * 
+	 * @return Api
+	 */
+	public static function withClient($clientId, $clientSecret)
+	{
+		$api = new static();
+		$api->clientId		= $clientId;
+		$api->clientSecret	= $clientSecret;
+		return $api;
 	}
 
 	/**
@@ -79,10 +124,23 @@ class Api
 	 * 
 	 * @return Api
 	 */
-	public static function byToken($token)
+	public static function withToken($token)
 	{
 		$api = new static();
 		$api->token = $token;
 		return $api;
+	}
+
+	/**
+	 * Set or get default cache class for this session
+	 * 
+	 * @param string|null	$defaultCacheClass	cache full classname
+	 * 
+	 * @return string
+	 */
+	public static function defaultCacheClass($defaultCacheClass = null)
+	{
+		if ($defaultCacheClass) static::$defaultCacheClass = $defaultCacheClass;
+		return static::$defaultCacheClass;
 	}
 }
